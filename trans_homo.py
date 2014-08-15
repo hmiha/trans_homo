04a291844c0d0d2af8f9a20c7f81f3293b3ec473
hmiha <hajimemihara1108@gmail.com>
2014-08-15 22:22:14 +0900

    ver1.0

--- trans_homo.py
+++ trans_homo.py
@@ -17,42 +17,46 @@ imgName = args.n
 d_h = int(args.dh)
 d_w = int(args.dw)
 
-
 # -- Global Valiables --
 it = 0 
 src_pt = np.zeros((4,2))
 
-
-# -- Draw circle in selected point
-
 def homographySolver(src_pt, dst_pt):
 
+    # -- definition -- 
     s = np.zeros((4, 3)) 
     d = np.zeros((4, 3)) 
     A = np.zeros((8, 9)) 
     b = np.zeros((9)) 
   
+    # -- --
     for i in range(4):
         s[i][:] = (src_pt[i][0], src_pt[i][1], 1)
         d[i][:] = (dst_pt[i][0], dst_pt[i][1], 1)
 
+    # -- create equation --
     for i in range(4):
         A[ 2 * i][:] = ((s[i][0], s[i][1], 1, 0, 0, 0, -d[i][0]*s[i][0], -d[i][0]*s[i][1], -d[i][0]))
         A[ 2 * i + 1][:] = ((0, 0, 0, s[i][0], s[i][1], 1, -d[i][1]*s[i][0], -d[i][1]*s[i][1], -d[i][1]))
 
+    # -- eigenvalue expand --
     l,v = np.linalg.eig(np.dot(A.T, A))
 
+    # -- get the index of minimum eigenvalue -- 
     i = np.argmin(l)
-
     
+    # -- normalization --
     for j in range(9):
         v[j][i] = v[j][i]/v[8][i]
 
+    # -- reshape --
     H = v[:,i].reshape((3,3))
 
-    print "Homography Matrix"
+    print "** Homography Matrix **"
     print H
     return H
+
+# -- Draw circle in selected point --
 
 def circleDrawer(x, y, src_img):
 
@@ -60,6 +64,7 @@ def circleDrawer(x, y, src_img):
     r = 10
     color = (255, 0, 0)
     cv2.circle(src_img, center, r, color)
+    return
     
 # -- on mouse function
 def onMouse(event, x, y, flags, param):
@@ -78,6 +83,7 @@ def onMouse(event, x, y, flags, param):
         it += 1
        
         cv2.imshow(windowName, src_img)
+    return
 
 def pointDisplay(pt):
 
@@ -85,6 +91,7 @@ def pointDisplay(pt):
     print "Upper-right: (", pt[1][0] , pt[1][1], ")"
     print "Lower-right: (", pt[2][0] , pt[2][1], ")"
     print "Lower-left:  (", pt[3][0] , pt[3][1], ") \n"
+    return
 
 def getDstPoints(d_h, d_w):
 
@@ -100,25 +107,22 @@ def getDstPoints(d_h, d_w):
 def getHomography(src_pt, dst_pt):
 
     hcv = homographySolver(src_pt, dst_pt)
-    #hcv_cv, mask = cv2.findHomography(src_pt, dst_pt)
-    #print "cv"
-    #print hcv_cv
 
     src_img = cv2.imread(imgName, 1)
     dst_img = cv2.warpPerspective(src_img, hcv, (d_w, d_h))
-    #dst_img = cv2.warpPerspective(src_img, hcv, (4, 4))
 
     cv2.namedWindow("dst")
     cv2.imshow("dst", dst_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    return 
 
 if __name__ == "__main__":
 
     src_img = cv2.imread(imgName, 1)
 
     # -- display usage --
-    usage = "Click points from upper-left points of object. \n"
+    usage = "Click corners from lower-left points. \n"
     usage = usage + "After select 4 points, press any key. \n"
     print(usage)
 
@@ -130,37 +134,21 @@ if __name__ == "__main__":
     if it <= 3:
         cv2.setMouseCallback(windowName, onMouse)
 
-    """
-    src_pt[0][:] = 1.0, 1.0 
-    src_pt[1][:] = 1.0, -1.0 
-    src_pt[2][:] = -1.0, 1.0 
-    src_pt[3][:] = -1.0, -1.0 
-    """
-
     cv2.imshow(windowName, src_img)
     cv2.waitKey(0)
 
     # -- after get 4 points --
 
-    print "** Sourse Points ** "
+    print "** Selected Points ** "
     pointDisplay(src_pt)
 
     cv2.destroyAllWindows()
 
     dst_pt = getDstPoints(d_h, d_w)
-    """
-    dst_pt = np.array([
-        [4.0  , 4.0],
-        [4.0  , -4.0],
-        [-4.0 , 4.0],
-        [-4.0 , -4.0]
-        ])
-    """
 
-
-
-
-    print "** Translated Points ** "
+    print "** Destination Points ** "
     pointDisplay(dst_pt)
 
     getHomography(src_pt, dst_pt)
+
+    # -- END --
